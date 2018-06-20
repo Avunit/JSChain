93036f46f2688bf61b01dee8792d59ae660ff98e
John Hayes <john@cutiep.ie>
2018-06-20 22:01:02 +0100

    Additional functions

--- jschain.js
+++ jschain.js
@@ -18,3 +18,15 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
 
 	return newBlock;
 }
+
+Blockchain.prototype.getLastBlock = function() {
+	return this.chain[this.chain.length -1];
+}
+
+Blockchain.prototype.createNewTransaction = function() {
+	const newTransaction = {
+		quantity:
+		sender:
+		recipient:	
+	}
+}
