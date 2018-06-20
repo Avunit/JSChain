a1c301017f65168c992f1c5c09e0c233839bea49
John Hayes <john@cutiep.ie>
2018-06-20 22:20:01 +0100

    Basic endpoint structure

--- api.js
+++ api.js
@@ -1,2 +1,18 @@
 const express = require('express');
 const app = express();
+
+app.get('/blockchain', function(req, res) {
+
+});
+
+app.post('/transaction', function(req, res) {
+
+});
+
+app.post('/mine', function(req, res) {
+
+});
+
+app.listen(3000, function() {
+	console.log(' --| Listening |-- ');
+});
